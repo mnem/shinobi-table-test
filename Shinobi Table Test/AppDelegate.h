b4d8cb02c3baa6648f7f08ec013bb415eb723363
David Wagner <mnem@noiseandheat.com>
2015-11-03 20:43:48 +0000

    Initial Commit

---
+++ Shinobi Table Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shinobi Table Test
+//
+//  Created by David Wagner on 03/11/2015.
+//  Copyright © 2015 David Wagner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shinobi Table Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shinobi Table Test
+//
+//  Created by David Wagner on 03/11/2015.
+//  Copyright © 2015 David Wagner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
